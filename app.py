8964619a314b920f6e9bd9db01ff2ecf6f83b1ce
Mario <matomario8@gmail.com>
2021-04-24 22:03:16 -0400

    update authors radio btn

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import piexif
 import csv
 from datetime import datetime as dt
 
-from tkinter import Tk, Label, Button, Checkbutton, IntVar, filedialog
+from tkinter import Tk, Label, Button, Checkbutton, Radiobutton, IntVar, Frame, filedialog
 from tkinter.ttk import Progressbar
 
 from pprint import pprint
@@ -116,83 +116,108 @@ def parse_time(string):
 
     return time
 
-class Gui:
+class MainWindow(Frame):
 
-    def __init__(self, root):
+    def __init__(self, tkinter_root=None):
 
-        # Other variables
-        self.directory = None
+        self.tkinter_root = tkinter_root 
 
-        # GUI variables
-        self.root = root
-        self.root.title("Mass Exif Editor")
+        self.help_text = Label(self.tkinter_root, text="Choose a folder and then hit run")
+        # self.help_text.pack()
+        self.help_text.grid(column=0, columnspan=2, row=0)
 
-        self.help_text = Label(self.root, text="Choose a folder and then hit run")
-        self.help_text.pack()
+        self.path_label = Label(self.tkinter_root, text="/")
+        # self.path_label.pack()
+        self.path_label.grid(column=0, columnspan=2, row=1)
 
-        self.path_label = Label(self.root, text="")
-        self.path_label.pack()
-
-        self.folder_button = Button(self.root, text="Choose folder",
+        self.folder_button = Button(self.tkinter_root, text="Choose folder",
                                     command=self.choose_file)
-        self.folder_button.pack()
+        # self.folder_button.pack()
+        self.folder_button.grid(column=0, columnspan=2, row=2, pady=10)
+
+        self.selected_option = IntVar(value=1)
+        self.default_radiobtn = Radiobutton(self.tkinter_root, text="Default",
+                                               variable=self.selected_option,
+                                               value=1)
+        # self.default_radiobtn.pack()
+        self.default_radiobtn.grid(column=0, row=3, padx=5, sticky="W")
 
         self.ignoreincomplete = IntVar()
-        self.ignore_checkbox = Checkbutton(self.root, text="Ignore incomplete dates",
+        self.ignore_checkbox = Checkbutton(self.tkinter_root, text="Ignore incomplete dates",
                                            variable=self.ignoreincomplete)
-        self.ignore_checkbox.pack()
+        # self.ignore_checkbox.pack()
+        self.ignore_checkbox.grid(column=1, row=3, padx=5)
 
-        self.titlesonly = IntVar()
-        self.titlesonly_checkbox = Checkbutton(self.root, text="Replace titles only",
-                                               variable=self.titlesonly)
-        self.titlesonly_checkbox.pack()
+        self.titlesonly_radiobtn = Radiobutton(self.tkinter_root, text="Replace titles",
+                                               variable=self.selected_option,
+                                               value=2)
+        # self.titlesonly_radiobtn.pack()
+        self.titlesonly_radiobtn.grid(column=0, row=4, padx=5, sticky="W")
 
-        self.csvexport = IntVar()
-        self.csvexport_checkbox = Checkbutton(self.root, text="CSV Export",
-                                              variable=self.csvexport)
+        self.authorsonly_radiobtn = Radiobutton(self.tkinter_root, text="Update authors (author;title)",
+                                               variable=self.selected_option,
+                                               value=3)
+        # self.authorsonly_radiobtn.pack()
+        self.authorsonly_radiobtn.grid(column=0, row=5, padx=5, sticky="W")
 
-        self.csvexport_checkbox.pack()
+        self.csvexport_radiobtn = Radiobutton(self.tkinter_root, text="CSV Export",
+                                              variable=self.selected_option,
+                                              value=4)
+        # self.csvexport_radiobtn.pack()
+        self.csvexport_radiobtn.grid(column=0, row=6, padx=5, sticky="W")
 
-        self.progress_bar = Progressbar(self.root, orient="horizontal",
+        self.progress_bar = Progressbar(self.tkinter_root, orient="horizontal",
                                         mode="determinate")
-        self.progress_bar.pack(pady=10)
+        # self.progress_bar.pack(pady=10)
+        self.progress_bar.grid(column=0, columnspan=2, row=7, sticky="EW", padx=40, pady=10)
 
-        self.run_button = Button(self.root, text="Run Tool",
+        self.run_button = Button(self.tkinter_root, text="Run Tool",
                                  command=self.update_images)
-        self.run_button.pack()
+        # self.run_button.pack()
+        self.run_button.grid(column=0, row=8, sticky="EW", padx=5, pady=5)
 
-        self.close_button = Button(self.root, text="Quit",
-                                   command=self.root.quit)
-        self.close_button.pack()
+        self.close_button = Button(self.tkinter_root, text="Quit",
+                                   command=self.tkinter_root.quit)
+        # self.close_button.pack()
+        self.close_button.grid(column=1, row=8, sticky="EW", padx=5, pady=5)
 
     def choose_file(self):
         self.directory = filedialog.askdirectory(title="Choose folder")
         self.path_label["text"] = self.directory
         self.progress_bar["value"] = 0
 
-
-    def update_images(self):
-
+    def run_tool(self, func):
         if self.directory is None:
+            # Could add a warning that no directory is selected (don't use a dialog)
             return
-
+        
         files = os.listdir(self.directory)
 
         self.progress_bar.length = len(files)
         self.progress_bar["value"] = 0
-        self.root.update_idletasks()
+        self.tkinter_root.update_idletasks()
+
+        if self.selected_option == 4:
+            print("Generating CSV")
+            self.generate_csv(files)
+        elif self.selected_option == 3:
+            print("Updating authors")
+            self.update_authors(files)
+        elif self.selected_option == 2:
+            print("Replacing titles")
+            self.update_titles(files)
+        else:
+            print("Default selected")
+            self.update_images(files)
+
+    def generate_csv(self, files):
+        # One of three commands to run the tool with (Create a CSV of all images and their EXIF data)
 
         for file in files:
-
             self.progress_bar["value"] += (100 / self.progress_bar.length)
-            self.root.update_idletasks()
+            self.tkinter_root.update_idletasks()
 
             filepath = Path(self.directory) / file
-            title_field = file.split(".", 1)[0]
-            
-
-
-            #Not a jpg, not able to open file, or no exif data, then continue to next file
             if not filepath.suffix.lower().endswith(('.jpg', '.jpeg')):
                 continue
 
@@ -205,91 +230,168 @@ class Gui:
             if "exif" not in im.info.keys():
                 continue
             
-            #Add all exif data for the image to a dictionary
+            exif_dict = piexif.load(im.info["exif"])
+
+            row_filename = file
+            row_title = exif_dict['0th'].get(piexif.ImageIFD.ImageDescription, "")
+            row_subject = decode_bytes(exif_dict['0th'].get(piexif.ImageIFD.XPSubject, ""))
+            row_author = bytetostring(exif_dict['0th'].get(piexif.ImageIFD.Artist, ""))
+            row_comment = decode_bytes(exif_dict['0th'].get(piexif.ImageIFD.XPComment, ""))
+
+            row_datetaken = bytetostring(exif_dict['Exif'].get(piexif.ExifIFD.DateTimeOriginal, ""))
+            row_datetime_obj = dt.strptime(row_datetaken, '%Y:%m:%d %H:%M:%S')
+            row_datetaken = row_datetime_obj.strftime('%H:%M:%S, %m/%d/%Y')
+
+            row_keywords = decode_bytes(exif_dict['0th'].get(piexif.ImageIFD.XPKeywords, ""))
+            row_gps = exif_dict.get('GPS', "")
+
+            if row_gps:
+                gps_lat = row_gps.get(2)
+                gps_long = row_gps.get(4)
+                gps_alt = row_gps.get(6)
+
+                if gps_lat and gps_long:
+                    gps_lat1 = gps_lat[0][0] / gps_lat[0][1]
+                    gps_lat2 = gps_lat[1][0] / gps_lat[1][1]
+                    gps_lat3 = gps_lat[2][0] / gps_lat[2][1]
+
+                    gps_long1 = gps_long[0][0] / gps_long[0][1]
+                    gps_long2 = gps_long[1][0] / gps_long[1][1]
+                    gps_long3 = gps_long[2][0] / gps_long[2][1]
+
+                    gps_lat = gps_lat1 + gps_lat2 / 60 + gps_lat3 / 3600
+                    gps_long = gps_long1 + gps_long2 / 60 + gps_long3 / 3600
+                    row_gps = str(gps_lat) + ", " + str(gps_long)
+
+                if gps_alt:
+                    gps_alt = gps_alt[0] / gps_alt[1]
+                    row_gps = row_gps + ", " + str(gps_alt)
+
+            row_string = [row_filename, row_title, row_subject, row_author,
+                        row_comment, row_datetaken, row_keywords, row_gps]
+            try:
+                csv_data.append(row_string)
+            except NameError:
+                csv_data = []
+                csv_data.append(row_string)
+
+
+        current_datetime = str(dt.now().strftime("%H.%M.%S %m-%d-%Y"))
+        with open(self.directory + "/" + current_datetime + "-export.csv", "wt", newline="") as csv_export:
+            writer = csv.writer(csv_export)
+            writer.writerow(["Filename", "Title", "Subject", "Author(s)", "Comments", "Date Taken", "Keywords", "GPS"])
+
+            for csv_row in csv_data:
+                writer.writerow(csv_row)
+
+    def update_titles(self, files):
+
+        for file in files:
+            self.progress_bar["value"] += (100 / self.progress_bar.length)
+            self.tkinter_root.update_idletasks()
+
+            filepath = Path(self.directory) / file
+            if not filepath.suffix.lower().endswith(('.jpg', '.jpeg')):
+                continue
+
+            try:
+                im = Image.open(filepath)
+            except (OSError):
+                print("Could not open/read file: ", filepath)
+                continue
+
+            if "exif" not in im.info.keys():
+                continue
+            
+            exif_dict = piexif.load(im.info["exif"])
+
+            title_field = file.split(".", 1)[0]
+            exif_dict["0th"].update({piexif.ImageIFD.ImageDescription: title_field})
+
+            exif_bytes = piexif.dump(exif_dict)
+            piexif.insert(exif_bytes, im.filename)
+
+    def update_authors(self, files):
+
+        for file in files:
+            self.progress_bar["value"] += (100 / self.progress_bar.length)
+            self.tkinter_root.update_idletasks()
+
+            filepath = Path(self.directory) / file
+            if not filepath.suffix.lower().endswith(('.jpg', '.jpeg')):
+                continue
+
+            try:
+                im = Image.open(filepath)
+            except (OSError):
+                print("Could not open/read file: ", filepath)
+                continue
+
+            if "exif" not in im.info.keys():
+                continue
+            
+            exif_dict = piexif.load(im.info["exif"])
+
+            title_field = file.split(".", 1)[0]
+            author_value = exif_dict['0th'].get(piexif.ImageIFD.XPAuthor, "") + ";" + title_field
+            exif_dict["0th"].update({piexif.ImageIFD.XPAuthor: author_value})
+
+            exif_bytes = piexif.dump(exif_dict)
+            piexif.insert(exif_bytes, im.filename)
+
+    def update_images(self, files):
+
+        for file in files:
+
+            self.progress_bar["value"] += (100 / self.progress_bar.length)
+            self.tkinter_root.update_idletasks()
+
+            filepath = Path(self.directory) / file      
+
+            # Not a jpg, not able to open file, or no exif data, then continue to next file
+            if not filepath.suffix.lower().endswith(('.jpg', '.jpeg')):
+                continue
+
+            try:
+                im = Image.open(filepath)
+            except (OSError):
+                print("Could not open/read file: ", filepath)
+                continue
+
+            if "exif" not in im.info.keys():
+                continue
+            
+            # Add all exif data for the image to a dictionary
             exif_dict = piexif.load(im.info["exif"])
             
             """
+        
             try:
                 exif_dict["0th"][piexif.ImageIFD.ImageDescription] = title_field
             except KeyError:
                 exif_dict["0th"].add(piexif.ImageIFD.ImageDescription, title_field)
             """
-            exif_dict["0th"].update({piexif.ImageIFD.ImageDescription: title_field})
-            if not self.titlesonly.get():
-                try:
-                    key = "Comments"
-                    comments_field = decode_bytes(exif_dict["0th"][piexif.ImageIFD.XPComment])
-                    date_string = str(parse_date(comments_field, 
-                                    ignoreincomplete=self.ignoreincomplete.get()))
-                    if date_string:
-                        time_string = str(parse_time(comments_field))
-                        datetime = date_string + " " + time_string
+        
+            try:
+                key = "Comments"
+                comments_field = decode_bytes(exif_dict["0th"][piexif.ImageIFD.XPComment])
+                date_string = str(parse_date(comments_field, 
+                                ignoreincomplete=self.ignoreincomplete.get()))
+                if date_string:
+                    time_string = str(parse_time(comments_field))
+                    datetime = date_string + " " + time_string
 
-                        key = "DateTimeOriginal"
-                        exif_dict["Exif"].update({piexif.ExifIFD.DateTimeOriginal: datetime})
+                    key = "DateTimeOriginal"
+                    exif_dict["Exif"].update({piexif.ExifIFD.DateTimeOriginal: datetime})
 
-                        key = "DateTimeDigitized"
-                        exif_dict["Exif"].update({piexif.ExifIFD.DateTimeDigitized: datetime})
-                except KeyError:
-                    print("'{}' field not found for file: {} ".format(key, filepath))
+                    key = "DateTimeDigitized"
+                    exif_dict["Exif"].update({piexif.ExifIFD.DateTimeDigitized: datetime})
+            except KeyError:
+                print("'{}' field not found for file: {} ".format(key, filepath))
                 
             exif_bytes = piexif.dump(exif_dict)
             piexif.insert(exif_bytes, im.filename)
-
-            if self.csvexport.get():
-
-                row_filename = file
-                row_title = exif_dict['0th'].get(piexif.ImageIFD.ImageDescription, "")
-                row_subject = decode_bytes(exif_dict['0th'].get(piexif.ImageIFD.XPSubject, ""))
-                row_author = bytetostring(exif_dict['0th'].get(piexif.ImageIFD.Artist, ""))
-                row_comment = decode_bytes(exif_dict['0th'].get(piexif.ImageIFD.XPComment, ""))
-
-                row_datetaken = bytetostring(exif_dict['Exif'].get(piexif.ExifIFD.DateTimeOriginal, ""))
-                row_datetime_obj = dt.strptime(row_datetaken, '%Y:%m:%d %H:%M:%S')
-                row_datetaken = row_datetime_obj.strftime('%H:%M:%S, %m/%d/%Y')
-
-                row_keywords = decode_bytes(exif_dict['0th'].get(piexif.ImageIFD.XPKeywords, ""))
-                row_gps = exif_dict.get('GPS', "")
-
-                if row_gps:
-                    gps_lat = row_gps.get(2)
-                    gps_long = row_gps.get(4)
-                    gps_alt = row_gps.get(6)
-
-                    if gps_lat and gps_long:
-                        gps_lat1 = gps_lat[0][0] / gps_lat[0][1]
-                        gps_lat2 = gps_lat[1][0] / gps_lat[1][1]
-                        gps_lat3 = gps_lat[2][0] / gps_lat[2][1]
-
-                        gps_long1 = gps_long[0][0] / gps_long[0][1]
-                        gps_long2 = gps_long[1][0] / gps_long[1][1]
-                        gps_long3 = gps_long[2][0] / gps_long[2][1]
-
-                        gps_lat = gps_lat1 + gps_lat2 / 60 + gps_lat3 / 3600
-                        gps_long = gps_long1 + gps_long2 / 60 + gps_long3 / 3600
-                        row_gps = str(gps_lat) + ", " + str(gps_long)
-
-                    if gps_alt:
-                        gps_alt = gps_alt[0] / gps_alt[1]
-                        row_gps = row_gps + ", " + str(gps_alt)
-
-                row_string = [row_filename, row_title, row_subject, row_author,
-                            row_comment, row_datetaken, row_keywords, row_gps]
-                try:
-                    csv_data.append(row_string)
-                except NameError:
-                    csv_data = []
-                    csv_data.append(row_string)
-
-        if self.csvexport.get():
-            current_datetime = str(dt.now().strftime("%H.%M.%S %m-%d-%Y"))
-            with open(self.directory + "/" + current_datetime + "-export.csv", "wt", newline="") as csv_export:
-                writer = csv.writer(csv_export)
-                writer.writerow(["Filename", "Title", "Subject", "Author(s)", "Comments", "Date Taken", "Keywords", "GPS"])
-
-                for csv_row in csv_data:
-                    writer.writerow(csv_row)
-
+        
 class Time:
 
     def __init__(self, hour="00", minute="00", second="00", am=True):
@@ -387,6 +489,6 @@ class Date:
 
 if __name__ == "__main__":
 
-    root = Tk()
-    gui = Gui(root)
-    gui.root.mainloop()
+    tkinter_root = Tk()
+    main_window = MainWindow(tkinter_root)
+    main_window.tkinter_root.mainloop()
